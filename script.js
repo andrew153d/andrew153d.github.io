7b31374b72706e885a8dfb98729712686be7af75
andrew <andrewwisnergrow@gmail.com>
2025-02-15 13:16:22 -0500

    Mobile test

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 //index.js
 
-const hamburger = document.getElementById('hamburger'); 
-const menu = document.querySelector('.menu'); 
+const hamburger = document.getElementById('hamburger');
+const menu = document.querySelector('.menu');
 
-hamburger.addEventListener('click', function () { 
-    const hamIcon = this.querySelector('.hamburger-icon'); 
-    const crossIcon = this.querySelector('.cross-icon'); 
-    if (hamIcon.style.display === "none") { 
+hamburger.addEventListener('click', function () {
+    const hamIcon = this.querySelector('.hamburger-icon');
+    const crossIcon = this.querySelector('.cross-icon');
+    if (hamIcon.style.display === "none") {
         hamIcon.style.display = "inline-block"
         menu.style.display = "none"
         crossIcon.style.display = "none"
-    } 
-    else { 
+    }
+    else {
         crossIcon.style.display = "inline-block"
         hamIcon.style.display = "none"
         menu.style.display = "block"
-    } 
+    }
 });
 
 document.getElementById('download-btn').addEventListener('click', function () {
@@ -24,4 +24,21 @@ document.getElementById('download-btn').addEventListener('click', function () {
     link.href = resumeURL;
     link.download = 'resume.pdf';
     link.click();
-  });
+});
+
+
+function nextImage(button) {
+    const galleryContainer = button.parentElement;
+    const images = galleryContainer.querySelectorAll('img');
+    let currentIndex = 0;
+
+    images.forEach((img, index) => {
+        if (!img.classList.contains('hidden')) {
+            img.classList.add('hidden');
+            currentIndex = index;
+        }
+    });
+
+    const nextIndex = (currentIndex + 1) % images.length;
+    images[nextIndex].classList.remove('hidden');
+}
